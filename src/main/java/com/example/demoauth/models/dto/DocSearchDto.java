4794812aee190d383e660b0eaf515d76cbf13d46
Marzhan Izteleu <marzhanizteleuova@gmail.com>
2023-05-02 06:00:59 +0600

    add documents methods

---
+++ src/main/java/com/example/demoauth/models/dto/DocSearchDto.java
@@ -0,0 +1,35 @@
+package com.example.demoauth.models.dto;
+
+import com.example.demoauth.models.enums.CategoryCode;
+import com.example.demoauth.models.enums.StatusCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Schema(description = "DTO for docs search")
+public class DocSearchDto {
+    @Schema(description = "Docs incident date from filter", type = "string", pattern = "dd-MM-yyyy", example = "25-03-2022")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    Date dateFrom;
+
+    @Schema(description = "Docs incident date to filter", type = "string", pattern = "dd-MM-yyyy", example = "25-04-2022")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    Date dateTo;
+
+    @Schema(description = "Doc category filter", example = "STATEMENT")
+    CategoryCode category;
+
+    @Schema(description = "Doc status filter", example = "CANCELED")
+    StatusCode status;
+
+//    @Schema(description = "Query search for doc number")
+//    String query;
+}
---
+++ src/main/java/com/example/demoauth/utils/specification/DocSpec.java
@@ -0,0 +1,60 @@
+package com.example.demoauth.utils.specification;
+
+import com.example.demoauth.models.entity.BaseEntity;
+import com.example.demoauth.models.entity.Doc;
+import com.example.demoauth.models.entity.DocCategory;
+import com.example.demoauth.models.entity.DocStatus;
+import com.example.demoauth.models.enums.CategoryCode;
+import com.example.demoauth.models.enums.StatusCode;
+import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import java.util.Date;
+
+@UtilityClass
+public class DocSpec {
+    public Specification<Doc> userFilter(Long userId) {
+        return (r, cq, cb) -> cb.equal(r.get(Doc.Fields.user).get(BaseEntity.Fields.id), userId);
+    }
+
+    public Specification<Doc> categoryFilter(CategoryCode category) {
+        return (r, cq, cb) -> cb.equal(r.get(Doc.Fields.category).get(DocCategory.Fields.code),
+                category);
+    }
+
+    public Specification<Doc> statusFilter(StatusCode status) {
+        return (r, cq, cb) -> cb.equal(r.get(Doc.Fields.status).get(DocStatus.Fields.code), status);
+    }
+
+    public Specification<Doc> dateFilter(Date dateFrom, Date dateTo) {
+        return (r, cq, cb) -> cb.and(cb.greaterThanOrEqualTo(r.get(Doc.Fields.createdAt), dateFrom),
+                cb.lessThan(r.get(Doc.Fields.createdAt), DateUtils.addDays(dateTo, 1)));
+    }
+
+    public Specification<Doc> docOrderByCreatedDate() {
+        return (r, cq, cb) -> {
+            Path<Doc> field = r.get(Doc.Fields.createdAt);
+            switchDirection(Sort.Direction.DESC, cq, cb, field);
+            return cb.and();
+        };
+    }
+
+    private void switchDirection(
+            Sort.Direction direction, CriteriaQuery<?> cq, CriteriaBuilder cb, Path<?> field) {
+        switch (direction) {
+            case ASC:
+                cq.orderBy(cb.asc(field));
+                break;
+            case DESC:
+                cq.orderBy(cb.desc(field));
+                break;
+        }
+    }
+
+
+}
