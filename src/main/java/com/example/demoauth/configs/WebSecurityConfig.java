814b6ce3499ec66b8edd930417b119915a06a537
Marzhan Izteleu <marzhanizteleuova@gmail.com>
2023-05-02 22:07:03 +0600

    .antMatchers("/**").permitAll()

--- src/main/java/com/example/demoauth/configs/WebSecurityConfig.java
+++ src/main/java/com/example/demoauth/configs/WebSecurityConfig.java
@@ -70,6 +70,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.antMatchers("/api/auth/**").permitAll()
 				.antMatchers("/api/test/**").permitAll()
+				.antMatchers("/**").permitAll()
 				.antMatchers(SWAGGER_URLS).permitAll()
 				.anyRequest().authenticated();
 		
